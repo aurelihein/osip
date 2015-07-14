06bd089f12b0d911e14c0f05dabf22f8ae2af305
Aymeric Moizard <amoizard@gmail.com>
2015-07-14 19:33:52 +0200

    start updating to version 5.0.0

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
-# Copyright (C) 2006-2007 David Sugar, Tycho Softworks.
-#
-# This file is free software; as a special exception the author gives
-# unlimited permission to copy and/or distribute it, with or without
-# modifications, as long as this notice is preserved.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-WANT_AUTOCONF_2_5=1
-export WANT_AUTOCONF_2_5
-
-rm -rf autoconf auto*.cache libtool
-if test ! -d autoconf ; then mkdir autoconf ; fi
+#AM_VERSION="1.10"
+if ! type aclocal-$AM_VERSION 1>/dev/null 2>&1; then
+	# automake-1.10 (recommended) is not available on Fedora 8
+	AUTOMAKE=automake
+	ACLOCAL=aclocal
+else
+	ACLOCAL=aclocal-${AM_VERSION}
+	AUTOMAKE=automake-${AM_VERSION}
+fi
 
 libtoolize="libtoolize"
 for lt in glibtoolize libtoolize15 libtoolize14 libtoolize13 ; do
@@ -23,9 +17,10 @@ for lt in glibtoolize libtoolize15 libtoolize14 libtoolize13 ; do
         if test -x /usr/local/bin/$lt ; then
                 libtoolize=$lt ; break
         fi
+        if test -x /opt/local/bin/$lt ; then
+                libtoolize=$lt ; break
+        fi
 done
-$libtoolize --copy --force
-
 AUTOMAKE_FLAGS=""
 case $libtoolize in
 *glibtoolize)
@@ -33,66 +28,15 @@ case $libtoolize in
 	;;
 esac
 
-ACLOCALDIRS=""
-if test -d scripts ; then
-	ACLOCALDIRS="-I scripts" ; fi
-
-if test ! -z "$ACLOCAL" ; then
-        ACLOCALDIRS="$ACLOCALDIRS "${ACLOCAL}
-elif test ! -z "$ACLOCAL_FLAGS" ; then
-	ACLOCALDIRS="$ACLOCALDIRS $ACLOCAL_FLAGS" ; fi
-
-reconf=""
-if test -f ~/.configure ; then
-	reconf=`grep ^reconfig: ~/.configure | sed -e s/^reconfig://`
-elif test -f /etc/configure.conf ; then
-	reconf=`grep ^reconfig: /etc/configure.conf | sed -e s/^reconfig://` ; fi
-if test ! -z "$reconf" ; then ACLOCALDIRS="$ACLOCALDIRS $reconf" ; fi
-
-if [ -d ~/share/aclocal ] ; then
-    ACLOCALDIRS="$ACLOCALDIRS -I ~/share/aclocal"
-elif [ -d /usr/local/share/aclocal ] ; then
-    ACLOCALDIRS="$ACLOCALDIRS -I /usr/local/share/aclocal"
+if test -d /opt/local/share/aclocal ; then
+        ACLOCAL_ARGS="-I /opt/local/share/aclocal"
 fi
 
-if test ! -z "$1" -o ! -z "${AUTOCONF_SUFFIX}" ; then
-        ver="$1"
-else
-        for v in 2.53 2.57 ; do
-                if test -f /usr/bin/autoconf-$v ; then
-                        ver=$v
-                fi
-        done
-fi
-
-if test "$ver" = "2.53" -a -z "$AUTOMAKE_SUFFIX" ; then
-	if test -f /usr/bin/automake-1.5 ; then
-		AUTOMAKE_SUFFIX="-1.5"
-	fi
-fi
-
-aclocal${AUTOMAKE_SUFFIX} $ACLOCALDIRS
-
-if test -f /usr/bin/autoheader-$ver ; then
-        /usr/bin/autoheader-$ver
-else
-        autoheader${AUTOCONF_SUFFIX}
-fi
-
-automake${AUTOMAKE_SUFFIX} --add-missing --copy ${AUTOMAKE_FLAGS}
-
-if test -f /usr/bin/autoconf-$ver ; then
-	/usr/bin/autoconf-$ver
-else
-	autoconf${AUTOCONF_SUFFIX}
-fi
-rm -f config.cache
-
-# fix for some broken...
-
-if test -f /usr/bin/automake-1.4 ; then
-	if test -f ltmain.sh ; then
-		cp ltmain.sh autoconf/ltmain.sh
-	fi
-fi
+set -x
+rm -rf config.cache autom4te.cache libtool
+$libtoolize --copy --force
+$ACLOCAL $ACLOCAL_ARGS
+autoheader
+$AUTOMAKE --add-missing --copy  ${AUTOMAKE_FLAGS}
+autoconf
 
