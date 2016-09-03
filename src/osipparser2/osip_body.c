b9dd097b5b24f5ee54b0a8739e59641cd51b6ead
Aymeric Moizard <amoizard@gmail.com>
2016-09-03 15:58:06 +0200

    * fix bug report: sr #109132: Heap buffer overflow in *osip_body_to_str*    https://savannah.gnu.org/support/index.php?109132

--- src/osipparser2/osip_body.c
+++ src/osipparser2/osip_body.c
@@ -416,6 +416,14 @@ osip_body_to_str (const osip_body_t * body, char **dest, size_t * str_length)
   }
   
   if ((osip_list_size (body->headers) > 0) || (body->content_type != NULL)) {
+    if (length < tmp_body - ptr + 3) {
+      size_t len;
+
+      len = tmp_body - ptr;
+      length = length + 3 + body->length; /* add body->length, to avoid calling realloc often */
+      ptr = osip_realloc (ptr, length);
+      tmp_body = ptr + len;
+    }
     tmp_body = osip_strn_append (tmp_body, CRLF, 2);
   }
   if (length < tmp_body - ptr + body->length + 4) {
